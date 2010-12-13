3aa90563712a1462e9eb240a902fcf25739f36b8
Jens W. Klein <jens@bluedynamics.com>
2010-12-13 12:50:04 +0100

    state correct license

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 import sys, os
 
 version = '1.0'
-shortdesc = 'Autocomplete Widget for YAFOWIL - Yet Another Form Widget Library (Python, Web)'
+shortdesc = 'Tree Selection Widget for YAFOWIL - Yet Another Form Widget Library (Python, Web)'
 longdesc = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
 longdesc += open(os.path.join(os.path.dirname(__file__), 'HISTORY.rst')).read()
 longdesc += open(os.path.join(os.path.dirname(__file__), 'LICENSE.rst')).read()
@@ -17,13 +17,14 @@ setup(name='yafowil.widget.dynatree',
             'Environment :: Web Environment',
             'Operating System :: OS Independent',
             'Programming Language :: Python', 
-            'Topic :: Internet :: WWW/HTTP :: Dynamic Content',        
+            'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+            'License :: OSI Approved :: BSD License',
       ],
       keywords='',
       author='BlueDynamics Alliance',
       author_email='dev@bluedynamics.com',
       url=u'http://github.com/bluedynamics/yafowil.widget.dynatree',
-      license='GNU General Public Licence',
+      license='Simplified BSD',
       packages=find_packages('src'),
       package_dir = {'': 'src'},
       namespace_packages=['yafowil', 'yafowil.widget'],
