e76f6f0b80c989eee5d3f895586b8007bd655a3c
Robert Niederreiter <office@squarewave.at>
2012-06-11 23:47:30 +0200

    change resource providing

--- src/yafowil/widget/dynatree/__init__.py
+++ src/yafowil/widget/dynatree/__init__.py
@@ -1,20 +1,29 @@
 import os 
 
+
 def register():
     import widget
-    
+
+
 def get_resource_dir():
     return os.path.join(os.path.dirname(__file__), 'resources')
-        
-def get_js(thirdparty=True):
-    js = list()
-    if thirdparty:
-        js.append('jquery.dynatree/jquery.dynatree.min.js')
-    js.append('widget.js')
-    return js
 
-def get_css(thirdparty=True):
-    css = list()
-    if thirdparty:
-        css.append('jquery.dynatree/skin/ui.dynatree.css')
-    return css
+
+def get_js():
+    return [{
+        'resource': 'jquery.dynatree/jquery.dynatree.min.js',
+        'thirdparty': True,
+        'order': 20,
+    }, {
+        'resource': 'widget.js',
+        'thirdparty': False,
+        'order': 21,
+    }]
+
+
+def get_css():
+    return [{
+        'resource': 'jquery.dynatree/skin/ui.dynatree.css',
+        'thirdparty': True,
+        'order': 20,
+    }]
