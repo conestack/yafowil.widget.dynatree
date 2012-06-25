45678dc9a905c66f04ac5045cfe3cbb095ab1660
Robert Niederreiter <office@squarewave.at>
2012-06-25 17:22:09 +0200

    resources

--- src/yafowil/widget/dynatree/__init__.py
+++ src/yafowil/widget/dynatree/__init__.py
@@ -1,33 +1,47 @@
 import os
+from yafowil.base import factory
+
+
+resourcedir = os.path.join(os.path.dirname(__file__), 'resources')
+
+js = [{
+    'resource': 'jquery.dynatree/jquery.dynatree.min.js',
+    'thirdparty': True,
+    'order': 20,
+}, {
+    'resource': 'widget.js',
+    'thirdparty': False,
+    'order': 21,
+}]
+
+css = [{
+    'resource': 'jquery.dynatree/skin-bootstrap/ui.dynatree.css',
+    'thirdparty': True,
+    'order': 20,
+}, {
+    'resource': 'widget.css',
+    'thirdparty': False,
+    'order': 21,
+}]
 
 
 def register():
     import widget
+    factory.register_theme('default', 'yafowil.widget.dynatree',
+                           resourcedir, js=js, css=css)
 
+
+###############################################################################
+# XXX: outdated below
+###############################################################################
 
 def get_resource_dir():
-    return os.path.join(os.path.dirname(__file__), 'resources')
+    return resourcedir
 
 
 def get_js():
-    return [{
-        'resource': 'jquery.dynatree/jquery.dynatree.min.js',
-        'thirdparty': True,
-        'order': 20,
-    }, {
-        'resource': 'widget.js',
-        'thirdparty': False,
-        'order': 21,
-    }]
+    return js
 
 
 def get_css():
-    return [{
-        'resource': 'jquery.dynatree/skin-bootstrap/ui.dynatree.css',
-        'thirdparty': True,
-        'order': 20,
-    }, {
-        'resource': 'widget.css',
-        'thirdparty': False,
-        'order': 21,
-    }]
+    return css
