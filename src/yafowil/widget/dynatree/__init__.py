4b34a69d88e9e221c6b8d61a945fb8cfbf31faa4
Robert Niederreiter <office@squarewave.at>
2012-09-28 10:49:05 +0200

    blanklines

--- src/yafowil/widget/dynatree/__init__.py
+++ src/yafowil/widget/dynatree/__init__.py
@@ -2,9 +2,7 @@ import os
 from yafowil.base import factory
 
 
-# XXX: use fanstatic
 resourcedir = os.path.join(os.path.dirname(__file__), 'resources')
-
 js = [{
     'group': 'dynatree',
     'resource': 'jquery.dynatree/jquery.dynatree.min.js',
@@ -14,7 +12,6 @@ js = [{
     'resource': 'widget.js',
     'order': 21,
 }]
-
 default_css = [{
     'group': 'dynatree',
     'resource': 'jquery.dynatree/skin/ui.dynatree.css',
@@ -24,7 +21,6 @@ default_css = [{
     'resource': 'widget.css',
     'order': 21,
 }]
-
 bootstrap_css = [{
     'group': 'dynatree',
     'resource': 'jquery.dynatree/skin-bootstrap/ui.dynatree.css',
