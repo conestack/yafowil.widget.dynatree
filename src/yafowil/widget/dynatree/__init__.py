abb17cc9a29f650f36fde642de3df9cc9a147070
Robert Niederreiter <office@squarewave.at>
2012-10-03 10:49:30 +0200

    resource group names

--- src/yafowil/widget/dynatree/__init__.py
+++ src/yafowil/widget/dynatree/__init__.py
@@ -4,29 +4,29 @@ from yafowil.base import factory
 
 resourcedir = os.path.join(os.path.dirname(__file__), 'resources')
 js = [{
-    'group': 'dynatree',
+    'group': 'yafowil.widget.dynatree.dependencies',
     'resource': 'jquery.dynatree/jquery.dynatree.min.js',
     'order': 20,
 }, {
-    'group': 'yafowil.widget.dynatree',
+    'group': 'yafowil.widget.dynatree.common',
     'resource': 'widget.js',
     'order': 21,
 }]
 default_css = [{
-    'group': 'dynatree',
+    'group': 'yafowil.widget.dynatree.dependencies',
     'resource': 'jquery.dynatree/skin/ui.dynatree.css',
     'order': 20,
 }, {
-    'group': 'yafowil.widget.dynatree',
+    'group': 'yafowil.widget.dynatree.common',
     'resource': 'widget.css',
     'order': 21,
 }]
 bootstrap_css = [{
-    'group': 'dynatree',
+    'group': 'yafowil.widget.dynatree.dependencies',
     'resource': 'jquery.dynatree/skin-bootstrap/ui.dynatree.css',
     'order': 20,
 }, {
-    'group': 'yafowil.widget.dynatree',
+    'group': 'yafowil.widget.dynatree.common',
     'resource': 'widget.css',
     'order': 21,
 }]
